d60a2c7be4ce968d8dd99da68aefab585ee8e66e
Abhinav Singh <abhinav.foss@gmail.com>
2020-08-15 16:02:50 +0000

    Testcases will fail now

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<App12 />);
+  const linkElement = getByText(/learn1234 react/i);
   expect(linkElement).toBeInTheDocument();
 });
